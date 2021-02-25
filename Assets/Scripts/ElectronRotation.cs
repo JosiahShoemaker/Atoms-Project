1913d2a7e5094a37178c5ed927175b764be55727
Josiah <jbshoes722@gmail.com>
2021-02-25 18:58:57 -0500

    Initial commit

---
+++ Assets/Scripts/ElectronRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronRotation : MonoBehaviour
+{
+
+    public GameObject[] electronPivotArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        electronPivotArray[0].transform.Rotate(new Vector3(0, 1, 0));
+        electronPivotArray[1].transform.Rotate(new Vector3(0, 1, 0));
+
+    }
+}
